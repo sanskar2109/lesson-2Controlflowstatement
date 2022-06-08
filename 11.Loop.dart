504c80c511cb97e32a195c6401533840d92e877b
Sanskar Singhal <89839415+sanskar2109@users.noreply.github.com>
2022-06-08 11:04:28 +0530

    Initial commit

---
+++ 11.Loop.dart
@@ -0,0 +1,7 @@
+void main()
+{
+  for (int i=1;i<=5;i++)
+    {
+      print("Loop-$i");
+    }
+}
---
+++ 9.IfStatement.dart
@@ -0,0 +1,15 @@
+import 'dart:io';
+
+void main()
+{
+  stdout.write("20 + 15:");
+  int num= int.parse(stdin.readLineSync()!);
+
+  if(num==35) {
+    print("Correct Result!");
+  }
+    else {
+      print("Incorrect Result!");
+  }
+
+}
